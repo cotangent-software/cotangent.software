701c94c700be73de40fed276794072772e93e7aa
Connor Burns <ctrekker4@gmail.com>
2020-08-31 10:18:49 -0600

    #5 Contact form error handling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,6 +11,8 @@ function Contact({ data }) {
   const [ organization, setOrganization ] = useState('');
   const [ message, setMessage ] = useState('');
   
+  const [ errorText, setErrorText ] = useState('');
+  
   function handleContactSubmit(e) {
     const myHeaders = new Headers();
     myHeaders.append('pragma', 'no-cache');
@@ -23,7 +25,10 @@ function Contact({ data }) {
       body: JSON.stringify({ name, email, organization, message })
     })
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => console.log(data))
+      .catch(err => {
+        setErrorText('There was an error submitting your message. Please ensure you included all required fields');
+      });
   }
   
   return <Layout>
@@ -35,12 +40,13 @@ function Contact({ data }) {
         It may take up to 7 business days to receive a reply.
       </p>
       <h4 style={{ marginBottom: '10px' }}>Contact Form</h4>
-      <div role={'form'} style={{ marginBottom: '50px' }}>
+      <div role={'form'} style={{ marginBottom: '50px', width: '100%' }}>
         <div style={{ color: 'grey', marginBottom: '5px' }}>* = required</div>
         <InputText gutterBottom required onChange={(e) => setName(e.target.value)} placeholder={'Name'}/>
         <InputText gutterBottom required onChange={(e) => setEmail(e.target.value)} placeholder={'Email'}/>
         <InputText gutterBottom onChange={(e) => setOrganization(e.target.value)} placeholder={'Organization'}/>
         <InputText gutterBottom rows={5} cols={50} required onChange={(e) => setMessage(e.target.value)} placeholder={'Message'}/>
+        <span style={{ color: 'red' }}>{ errorText }</span>
         <Button onClick={handleContactSubmit}>Submit</Button>
       </div>
     </div>
