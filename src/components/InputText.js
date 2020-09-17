907451a39b192a1d29daadcf70159df75e47226d
Connor Burns <ctrekker4@gmail.com>
2020-09-16 19:52:54 -0600

    Mobile-friendly products grid

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function InputText({ required, onChange, placeholder, label, rows=null, cols=null, gutterBottom }) {
+function InputText({ required, onChange, placeholder, label, rows=null, cols=null, gutterBottom, style }) {
   const placeholderVal = placeholder + (required ? ' *' : '');
   
-  let inputEl = <input type={'text'} placeholder={placeholderVal} onChange={onChange}/>;
+  let inputEl = <input style={style} type={'text'} placeholder={placeholderVal} onChange={onChange}/>;
   if(rows && cols) {
-    inputEl = <textarea style={{ resize: 'none' }} rows={rows} cols={cols} placeholder={placeholderVal} onChange={onChange}/>
+    inputEl = <textarea style={{ resize: 'none', ...style }} rows={rows} cols={cols} placeholder={placeholderVal} onChange={onChange}/>
   }
   return (
     <div style={{ marginBottom: gutterBottom ? '10px' : 0}}>
